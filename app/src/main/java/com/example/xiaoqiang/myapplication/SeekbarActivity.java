6a0117ab63ee7a5661473d75b6735837ff9d811b
xiaoqiang <wangqiangqiang@kingsoft.com>
2017-12-27 21:01:15 +0800

    ColorFul Seek bar init

--- app/src/main/java/com/example/xiaoqiang/myapplication/SeekbarActivity.java
+++ app/src/main/java/com/example/xiaoqiang/myapplication/SeekbarActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.SeekBar;
 
 import com.example.xiaoqiang.myapplication.view.ColorFulSeekbar;
+import com.example.xiaoqiang.myapplication.view.ColorSeekbar;
 
 import static android.content.ContentValues.TAG;
 
@@ -24,25 +25,44 @@ public class SeekbarActivity extends Activity {
     private final static String TAG = SeekbarActivity.class.getName();
 
     private ColorFulSeekbar mSeekbar;
+    private ColorSeekbar mColorSeek;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seekbar);
         mSeekbar = (ColorFulSeekbar) findViewById(R.id.seek_bar);
-        mSeekbar.setOnSeekBarChangeListener(new ColorFulSeekbar.OnSeekBarChangeListener() {
+        mColorSeek = (ColorSeekbar) findViewById(R.id.color_seek_bar);
+//        mSeekbar.setOnSeekBarChangeListener(new ColorFulSeekbar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(ColorFulSeekbar seekBar, int progress, boolean fromUser) {
+//                Log.e(TAG, "onProgressChanged,progress:" + progress + ",fromUser:" + fromUser);
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(ColorFulSeekbar seekBar) {
+//                Log.e(TAG, "onStartTrackingTouch");
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(ColorFulSeekbar seekBar) {
+//                Log.e(TAG, "onStopTrackingTouch");
+//            }
+//        });
+
+        mColorSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(ColorFulSeekbar seekBar, int progress, boolean fromUser) {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 Log.e(TAG, "onProgressChanged,progress:" + progress + ",fromUser:" + fromUser);
             }
 
             @Override
-            public void onStartTrackingTouch(ColorFulSeekbar seekBar) {
+            public void onStartTrackingTouch(SeekBar seekBar) {
                 Log.e(TAG, "onStartTrackingTouch");
             }
 
             @Override
-            public void onStopTrackingTouch(ColorFulSeekbar seekBar) {
+            public void onStopTrackingTouch(SeekBar seekBar) {
                 Log.e(TAG, "onStopTrackingTouch");
             }
         });
@@ -51,30 +71,35 @@ public class SeekbarActivity extends Activity {
             @Override
             public void onClick(View v) {
                 mSeekbar.setProgress(0xff000000, mSeekbar.getProgress() + 1);
+                mColorSeek.setProgress(0xff000000, mSeekbar.getProgress() + 1);
             }
         });
         findViewById(R.id.add_blue).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mSeekbar.setProgress(0xff0000ff, mSeekbar.getProgress() + 1);
+                mColorSeek.setProgress(0xff0000ff, mSeekbar.getProgress() + 1);
             }
         });
         findViewById(R.id.add_red).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mSeekbar.setProgress(0xffff0000, mSeekbar.getProgress() + 1);
+                mColorSeek.setProgress(0xffff0000, mSeekbar.getProgress() + 1);
             }
         });
         findViewById(R.id.add_5).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mSeekbar.setProgress(mSeekbar.getProgress() + 5);
+                mColorSeek.setProgress(mSeekbar.getProgress() + 5);
             }
         });
         findViewById(R.id.remove_5).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mSeekbar.setProgress(mSeekbar.getProgress() - 5);
+                mColorSeek.setProgress(mSeekbar.getProgress() - 5);
             }
         });
     }
---
+++ app/src/main/java/com/example/xiaoqiang/myapplication/view/ColorSeekbar.java
@@ -0,0 +1,143 @@
+package com.example.xiaoqiang.myapplication.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+
+import com.example.xiaoqiang.myapplication.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: [xiaoqiang]
+ * @Description: [ColorSeekbar]
+ * @CreateDate: [2017/12/27]
+ * @UpdateDate: [2017/12/27]
+ * @UpdateUser: [xiaoqiang]
+ * @UpdateRemark: []
+ */
+
+public class ColorSeekbar extends android.support.v7.widget.AppCompatSeekBar {
+    private List<ColorScope> mColorList;
+    private Paint mLintPaint;
+    private RectF mLintRect;
+    private int mHeight;
+    private int mWidth;
+    private int mMaxHeight;
+    private int mMinHeight;
+    private int mBackground;
+    private RectF mBackRect;
+
+    public ColorSeekbar(Context context) {
+        this(context, null);
+    }
+
+    public ColorSeekbar(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ColorSeekbar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mLintPaint = new Paint();
+        mLintPaint.setAntiAlias(true); //消除锯齿
+        mLintPaint.setStrokeWidth(1f);
+        mLintPaint.setStyle(Paint.Style.FILL);
+        loadAttribute(attrs);
+    }
+
+
+    private final void loadAttribute(AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.ColorSeekbar);
+            mBackground = typedArray.getColor(R.styleable.ColorSeekbar_ColorBackground, 0x99000000);
+            mMaxHeight = (int) typedArray.getDimension(R.styleable.ColorSeekbar_ColorMaxHeight, mMaxHeight);
+            mMinHeight = (int) typedArray.getDimension(R.styleable.ColorSeekbar_ColorMinHeight, mMinHeight);
+            typedArray.recycle();
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mLintPaint.setColor(mBackground);
+        canvas.drawRoundRect(mBackRect, mHeight / 2, mHeight / 2, mLintPaint);
+        drawColorLint(canvas);
+        super.onDraw(canvas);
+    }
+
+    public synchronized void setProgress(int color, int progress) {
+        if (progress >= 0) {
+            if (mColorList == null) {
+                mColorList = new ArrayList<ColorScope>();
+                mColorList.add(new ColorScope(color, getProgress(), getProgress()));
+            }
+            ColorScope colorScope = mColorList.get(mColorList.size() - 1);
+            if (!(colorScope.mColor == color && getProgress() >= colorScope.mStartProgress
+                    && getProgress() <= colorScope.mEndProgress)) {
+                colorScope = new ColorScope(color, getProgress(), getProgress());
+                mColorList.add(colorScope);
+            }
+            colorScope.mEndProgress = progress;
+            setProgress(progress);
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+//        mLintRect = new RectF(getPaddingLeft(), getPaddingTop(),
+//                getWidth() - getPaddingRight(), getHeight() - getPaddingBottom());
+
+        mBackRect = new RectF(getPaddingLeft(), getHeight() / 2 - mHeight / 2,
+                getWidth() - getPaddingRight(), getHeight() / 2 + mHeight / 2);
+        mHeight = Math.max(mMinHeight, Math.min(mMaxHeight, getHeight()));
+
+        mLintRect = new RectF(getPaddingLeft(), getHeight() / 2 - mHeight / 2,
+                getWidth() - getPaddingRight(), getHeight() / 2 + mHeight / 2);
+        mWidth = getWidth() - getPaddingLeft() - getPaddingRight();
+    }
+
+    private synchronized void drawColorLint(Canvas canvas) {
+        if (mColorList != null) {
+            for (ColorScope scope : mColorList) {
+                mLintRect.left = progressToPx(scope.mStartProgress) + getPaddingLeft();
+                mLintRect.right = progressToPx(scope.mEndProgress) + getPaddingLeft();
+                mLintPaint.setColor(scope.mColor);
+                canvas.drawRoundRect(mLintRect, mHeight / 2, mHeight / 2, mLintPaint);
+            }
+        }
+    }
+
+    public class ColorScope {
+        public ColorScope(int color, int startProgress, int endProgress) {
+            this.mColor = color;
+            this.mStartProgress = startProgress;
+            this.mEndProgress = endProgress;
+        }
+
+        public int mColor;
+        public int mStartProgress;
+        public int mEndProgress;
+    }
+
+    private float progressToPx(int progress) {
+        float left = (float) progress / getMax() * mWidth + getPaddingLeft();
+        if (left < getPaddingLeft()) {
+            return getPaddingLeft();
+        } else if (left > (mWidth + getPaddingLeft())) {
+            return mWidth + getPaddingLeft();
+        }
+        return left;
+    }
+
+}
