a7ec71e1aa8e660212f4bce6d1bd66f8851ef62f
xiaoqiang <wangqiangqiang@kingsoft.com>
2018-04-19 20:31:19 +0800

    fix bug

--- app/src/main/java/com/example/xiaoqiang/myapplication/designMode/builder/BuilderActivity.java
+++ app/src/main/java/com/example/xiaoqiang/myapplication/designMode/builder/BuilderActivity.java
@@ -26,9 +26,9 @@ public class BuilderActivity extends Activity {
         StringBuffer stringBuffer = new StringBuffer();
 
         stringBuffer.append("通过建造者模式创建魅族手机对象\n");
-        stringBuffer.append(new Mobile.Builder().bindCell("魅族").bindCell("魅族").bindExterior("红色").build().toString());
+        stringBuffer.append(new Mobile.Builder().bindCpu("魅族").bindCell("魅族").bindExterior("红色").build().toString());
         stringBuffer.append("\n通过建造者模式创建小米手机对象\n");
-        stringBuffer.append(new Mobile.Builder().bindCell("小米").bindCell("小米").bindExterior("蓝色").build().toString());
+        stringBuffer.append(new Mobile.Builder().bindCpu("小米").bindCell("小米").bindExterior("蓝色").build().toString());
 
         mTextView.setText(stringBuffer.toString());
 
